ca67b0bdb046ad5fc7cf74466ff9be8837e7a750
wachuuski <61634580+wachuuski@users.noreply.github.com>
2022-01-10 09:46:42 -0800

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,3 @@
-console.log("hello, world")
+console.log("My name is Timothy Chien")
+console.log("I have had at least 4 intro to CS classes, none of which were good.")
+console.log("I'm excited to perhaps... learn something. CS is an important skill to have especially now. It'd be nice to competently program.")
