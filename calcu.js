339ae9c420ce9039779e02d1abeda168445e1901
wachuuski <61634580+wachuuski@users.noreply.github.com>
2022-01-12 09:45:39 -0800

    Update calcu.js

--- calcu.js
+++ calcu.js
@@ -1,4 +1,14 @@
 let prompt = require('prompt-sync')();
-let expression = prompt("input some text: ");
-let expressionArray = expression.split("+"||"-"||"*"||"/")
-console.log(expressionArray);
+let expression = prompt("input an expression: ");
+let expressionArray = expression.split(/(\W)/)
+if (expressionArray[1]="+"){
+    console.log(parseInt(expressionArray[0])+parseInt(expressionArray[2]))
+}else if (expressionArray[1]="-"){
+    console.log(parseInt(expressionArray[0])-parseInt(expressionArray[2]))
+}else if (expressionArray[1]="*"){
+    console.log(parseInt(expressionArray[0])*parseInt(expressionArray[2]))
+}else if (expressionArray[1]="/"){
+    console.log(parseInt(expressionArray[0])/parseInt(expressionArray[2]))
+}else {
+    console.log("you bowl of soggy raisin bran, input a numerical expression")
+}
