ff5e2b23e0531ac4ae8cfec8275befe08f14308d
wachuuski <61634580+wachuuski@users.noreply.github.com>
2022-01-16 23:38:06 -0800

    Update challenge1.js

--- challenge1.js
+++ challenge1.js
@@ -1,13 +1,14 @@
 // console.log(fs.readFileSync('PC1input.txt','utf8'))
-const fs = require ('fs')
+const fs = require ('fs');
 function spew(){
-    data = (fs.readFileSync('PC1input.txt','utf8').split("\n"))
+    data = (fs.readFileSync('PC1input.txt','utf8').split("\n"));
 }
+
 function tallyLift(){
     spew();
-    let higher = 0
+    let higher = 0;
     for (let i = 0; i<=data.length; i++){
-        if (data[i]<data[i+1]){
+        if (data[i]>data[i-1]){
             higher++;
         }
     }
@@ -15,7 +16,7 @@ function tallyLift(){
 }
 function leapCheck(){
     spew();
-    let jump = null
+    let jump = null;
     for (let i = 0; i<=data.length; i++){
         if (data[i+1]-data[i]>jump){
             jump = data[i+1]-data[i];
